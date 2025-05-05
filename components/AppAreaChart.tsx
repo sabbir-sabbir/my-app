9a3e55dc0872a6b5077deb4be8046825439338d2
sabbir-sabbir <iosabbir7890@gmail.com>
2025-05-05 12:24:13 +0600

    add

--- components/AppAreaChart.tsx
+++ components/AppAreaChart.tsx
@@ -49,32 +49,6 @@ const AppAreaChart = () => {
     />
     <ChartTooltip content={<ChartTooltipContent />} />
     <ChartLegend content={<ChartLegendContent />} />
-    {/* <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-            </defs> */}
     <Area
               dataKey="mobile"
               type="natural"
